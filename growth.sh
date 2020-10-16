442d99b2a19559d062bd37553a7e8fe69c89918c
mattfel <feldman.matthew1@gmail.com>
2020-10-16 09:57:09 -0700

    updoot

--- growth.sh
+++ growth.sh
@@ -6,7 +6,7 @@ for commit in $(git rev-list master --reverse)
 do
     num=`git --no-pager show $commit:metadata.json  | grep num_clicks | tail -1 | sed "s/.* //g"`
     time=`git show $commit -s --format=%ct`
-    if [[ "$num" -gt "$lastnum" ]]; then
+    if [[ "$num" -gt "$lastnum" && "$num" -gt "10000" ]]; then
       echo "$time,$num" >> growth.csv
       lastnum=$num
     fi
